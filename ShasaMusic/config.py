5a26dd315b33b68ca98ca54cc3ec0aaa3b4f597c
looserakki <70113298+looserakki@users.noreply.github.com>
2021-09-25 16:55:19 +0530

    Update config.py

--- ShasaMusic/config.py
+++ ShasaMusic/config.py
@@ -1,43 +1,23 @@
-# ShasaMusic- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
 import os
 from os import getenv
 
-from dotenv import load_dotenv
 
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "LionXUpdates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dc85bcdb2f27218033b8c.jpg")
+SESSION_NAME = getenv("SESSION_NAME", "BQCCJtbo92Z0XHqJ8vcf-4g1E26UMbts7PncmQpwGB2ZQUcXaA89prvS1gZTBPc8_H5VUerZYFSEvNXPG33kZNZ_XPz9nNYilgUjGN1roTLCoeKrjFbv750oocf1dUcQ6iO7NaCqcC8VRo6VyrLmaHALm1ypiRqBvdMQTOHLB42tjx_ZAPoQTHK9g6Voi7r4o_-0T6jzpoCizoWD1j_ZXSNlovyhrGfhBs0B9zjRWYf-YQOTmArEdlwFotiEGWQC8vYc260Fzl7jLSmee4VWk7vA6-ZEIXBbwdsndug9NLbUy__AKN2rN5DHfXtPO-W22WDmZWmnyvNpY1YAeWSOa1ZIck0dzQA")
+BOT_TOKEN = getenv("BOT_TOKEN", "1728730929:AAHpuAEOph7_QVA3kswuU2xcjYMuSwhZmu8")
+BOT_NAME = getenv("BOT_NAME", " MUSICBOT")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DeCodeBots")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/3365191a863f7fd4c2e85.jpg")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "NA")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "ShasaSupport")
-PROJECT_NAME = getenv("PROJECT_NAME", "ShasaMusic v4")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/MdNoor786/ShasaMusic")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+API_ID = int(getenv("API_ID", "5786603"))
+API_HASH = getenv("API_HASH", "a1354f206a4a05109d0fc916c0f150d0")
+BOT_USERNAME = getenv("BOT_USERNAME", "PATRICIA_ROBOT")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "PATRICIA_ASSISTANT")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DeCodeSupport")
+PROJECT_NAME = getenv("PROJECT_NAME", "Patricia v4")
+SOURCE_CODE = getenv("SOURCE_CODE", "t.me/piroXpower")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "70"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 PMMSG = getenv(
@@ -47,4 +27,4 @@ PMMSG = getenv(
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2012882227").split()))
